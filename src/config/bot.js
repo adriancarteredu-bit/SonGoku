33a79512c7aa9a5088c4d11ada21f4e21a7f9dde
adriancarteredu-bit <adrian.carter.edu@gmail.com>
2026-06-09 21:06:10 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Ticket Pro community",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 4, 
       },
     ],
   },
